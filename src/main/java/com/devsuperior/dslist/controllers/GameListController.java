b9ff8422a185521a63c9cb93435b40d5128f2923
Anderson Barg <andbarg@yahoo.com.br>
2025-01-24 20:49:49 -0300

    Consulta SQL e projection

--- src/main/java/com/devsuperior/dslist/controllers/GameListController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameListController.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dslist.dto.GameListDTO;
+import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.services.GameListService;
+import com.devsuperior.dslist.services.GameService;
 
 @RestController			//configura a classe como um controlador
 @RequestMapping(value = "/lists") 		//mapeia o recurso para ser acessado pela url definida
@@ -16,10 +19,19 @@ public class GameListController {
 	
 	@Autowired
 	private GameListService gameListService;
-	
+
+	@Autowired
+	private GameService gameService;
+
 	@GetMapping			//configura o serviço como um GET para http
 	public List<GameListDTO> findAll(){
 		List<GameListDTO> result = gameListService.findAll();
+		return result;
+	}
+	
+	@GetMapping(value = "/{listId}/games")			//configura o serviço como um GET para http
+	public List<GameMinDTO> findByList(@PathVariable Long listId){
+		List<GameMinDTO> result = gameService.findByList(listId);
 		return result;
 	}
 }
